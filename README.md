b569dacb7d21a53c5572b7260867f8852cb796e7
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-09-25 19:03:04 +0800

    add:(first)

---
+++ README.md
@@ -0,0 +1,26 @@
+# 个人算法练习库
+
+本项目为个人操作系统练习程序，与实现[蒋炎岩](https://jyywiki.cn)老师 NJU [操作系统原理课程](https://space.bilibili.com/202224425?spm_id_from=333.1391.0.0)部分代码的实现与分析。
+
+
+## 使用方法
+
+1. 使用 mac 或者 Linux 操作系统进行相关`.cpp`文件运行。
+2. 注释方式使用`Doxygen`注释风格：  -- [代码注释你写对了吗？ Doxygen 风格注释，一目了然!](https://blog.csdn.net/memoryfurong/article/details/149191128)
+``` C++
+/**
+ * @brief 计算两个整数的和。
+ * 这是一个更详细的描述，可以解释函数的用途、实现细节等。
+ * @param a 第一个整数。
+ * @param b 第二个整数。
+ * @return 两个整数的和。
+ */
+int add(int a, int b) {
+    return a + b;
+}
+```
+
+## 许可证
+
+- 本项目采用 MIT License，详见 [LICENSE](LICENSE) 文件。
+- 本仓库仅供个人学习和交流使用。
---
+++ pidFork.cpp
@@ -0,0 +1,36 @@
+
+/**
+ * @file pidFork.cpp
+ * @brief 演示 fork() 系统调用产生的进程状态及其 PID 关系。
+ *        本程序通过两次 fork()，展示了进程树的分叉及各进程中变量的取值。
+ * @see [05 - 程序与进程；进程管理 API [2025 南京大学操作系统原理]](https://www.bilibili.com/video/BV1fb97YsEWL/?spm_id_from=333.337.) 课程中 '1:07:50' 的题目 “fork：习题（1）
+ * @author [NotSleeply]
+ * @date 2025-09-25
+ */
+
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+/**
+ * @brief 执行两次 fork 并输出各进程的 PID 及 fork 返回值。
+ * @details 
+ * @note fork() 创建一个新进程，返回值为子进程的 PID（父进程中）或 0（子进程中）,类型为 pid_t。
+ * @note pid_t 是一个用于表示进程 ID 的数据类型，通常是一个整数类型。
+ *
+ */
+int main(void)
+{
+  pid_t x = fork();
+  pid_t y = fork();
+  printf("PID: %-5d | x: %-5d | y: %-5d\n", getpid(), x, y);
+}
+
+/**
+ * @example
+ * 运行结果示例（实际 PID 可能不同）：
+ * PID: 30387 | x: 30388 | y: 30389
+ * PID: 30389 | x: 30388 | y: 0
+ * PID: 30388 | x: 0     | y: 30390
+ * PID: 30390 | x: 0     | y: 0
+ */
